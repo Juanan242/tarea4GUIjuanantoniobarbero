387245e02c86e084bda0e105237375a5278aa0ca
Juanan242 <barberojuanan@gmail.com>
2023-04-03 21:04:56 +0200

    creada parte de la lógica para sumar, restar y mostrar el resultado en la igualacion

--- src/main/java/daw/PanelPrincipal.java
+++ src/main/java/daw/PanelPrincipal.java
@@ -22,11 +22,17 @@ public class PanelPrincipal extends JPanel implements ActionListener {
     private PanelBotones botonera;
     private JTextArea areaTexto;
     private int tipoOperacion;
+    private int operando1;
+    private int operando2;
+    private int resultado;
 
     // Constructor
     public PanelPrincipal() {
         initComponents();
         tipoOperacion = -1; // No hay operaciones en la calculadora
+        operando1 = 0;
+        operando2 = 0;
+        resultado = 0;
     }
 
     // Se inicializan los componentes gráficos y se colocan en el panel
@@ -44,25 +50,64 @@ public class PanelPrincipal extends JPanel implements ActionListener {
         // Colocamos la botonera y el área texto
         this.add(areaTexto, BorderLayout.NORTH);
         this.add(botonera, BorderLayout.SOUTH);
-        
-         for (JButton boton : this.botonera.getgrupoBotones()) {
+
+        for (JButton boton : this.botonera.getgrupoBotones()) {
             boton.addActionListener(this);
         }
 
     }
 
-     @Override
+    @Override
     public void actionPerformed(ActionEvent ae) {
-	// Se obtiene el objeto que desencadena el evento
+        // Se obtiene el objeto que desencadena el evento
         Object o = ae.getSource();
-	// Si es un botón
-        if (o instanceof JButton){
-            System.out.println(((JButton) o).getText());
-            areaTexto.setText(((JButton) o).getText());
-        }
-	
-	// RESTO DEL CÓDIGO DE LA LÓGICA DE LA CALCULADORA
-    }
 
-    
+        // Si es un botón
+//        if (o instanceof JButton){
+//            System.out.println(((JButton) o).getText());
+//            areaTexto.setText(((JButton) o).getText());
+//        }
+        //si es un botón
+        if (o instanceof JButton) {
+            String buttonText = ((JButton) o).getText();
+            switch (buttonText) {
+                case "+":
+                    //sumas
+                    tipoOperacion = 1;
+                    operando1 = (int) Double.parseDouble(areaTexto.getText());
+                    areaTexto.setText("");
+                    break;
+                case "-":
+                    //restas
+                    tipoOperacion = 2;
+                    operando1 = (int) Double.parseDouble(areaTexto.getText());
+                    areaTexto.setText("");
+                    break;
+                case "=":
+                    //resultado de los operando
+                    double operando2 = Double.parseDouble(areaTexto.getText());
+                    double resultado = 0.0;
+                    switch (tipoOperacion) {
+                        case 1:
+                             //sumas
+                            resultado = operando1 + operando2;
+                            break;
+                        case 2:
+                             //restas
+                            resultado = operando1 - operando2;
+                            break;
+                    }
+                
+                    areaTexto.setText(Double.toString(resultado));
+                    //reseteo la operacion
+                    tipoOperacion = -1; 
+                    break;
+
+                default:
+                     //por defecto los numeros
+                    areaTexto.setText(areaTexto.getText() + buttonText);
+                    break;
+            }
+        }
+    }
 }
